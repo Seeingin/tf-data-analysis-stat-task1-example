8d2481165ecf5cc733ad9fb3785243975dbca95c
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-03 17:53:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,7 @@ import numpy as np
 chat_id = 752592494
 
 def solution(x: np.array) -> float:
-    from scipy.optimize import minimize 
-
-    def log_like(alpha, x):
-      return np.sum(np.log(607 + np.random.lognormal(mean=np.log(alpha), sigma=1, size=len(x))) - x)
-
-    result = minimize(lambda alpha: -log_like(alpha, x), x0=1, method='BFGS')
-    return result.x[0]
+    mu = np.log(np.mean(x) - 607)
+    sigma = np.sqrt(np.log(1 + np.var(x) / np.power(np.mean(x) - 607, 2)))
+    alpha = np.exp(mu - sigma**2 / 2)
+    return alpha
