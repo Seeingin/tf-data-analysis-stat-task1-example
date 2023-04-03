8a11063fba92ca88b46daa1f2e5f99c994416991
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-03 17:50:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 752592494 # Ваш chat ID, не меняйте название переменной
+chat_id = 752592494
 
 def solution(x: np.array) -> float:
     from scipy.optimize import minimize 
