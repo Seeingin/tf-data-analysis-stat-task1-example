d0f9e12e4e9f36ea886f3b1bca95297b2ca90285
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-03 17:22:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 752592494 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    from scipy.optimize import minimize 
+
+    def log_like(alpha, x):
+      return np.sum(np.log(607 + np.random.lognormal(mean=np.log(alpha), sigma=1, size=len(x))) - x)
+
+    result = minimize(lambda alpha: -log_like(alpha, x), x0=1, method='BFGS')
+    return result.x[0]
