fe18ad0fe3bbc17b63bf9a41441330c3734ef1f6
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-04 11:58:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 752592494
 
 def solution(x: np.array) -> float:
-    from scipy.stats import norm
-    p = (x - 607).mean() / np.sqrt(np.var(x) / len(x))
-    alpha = 1 - p
+    loc = x.mean()
+    unbiased_variance = np.var(x, ddof=1)
+    alpha = np.log(unbiased_variance)
     return alpha
