5622ef702f8164b560d808cd5d4623cc99084306
Aleksandra Kuleva <129543447+Seeingin@users.noreply.github.com>
2023-04-10 11:04:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,17 +5,4 @@ import numpy as np
 chat_id = 752592494
 
 def solution(x: np.array) -> float:
-    from scipy.optimize import minimize
-    from scipy.stats import lognorm
-    
-    def log_likelihood(params, x):
-        alpha, sigma = params
-        mu = 607 + np.exp(alpha)
-        log_pdf = lognorm.logpdf(x, sigma, scale=mu)
-        return -np.sum(log_pdf)
-
-    initial_guess = [0.5, 1.0]
-    result = minimize(log_likelihood, initial_guess, args=(x,))
-    alpha = result.x[0]
-
-    return alpha
+    return np.log(x - 607).mean()
